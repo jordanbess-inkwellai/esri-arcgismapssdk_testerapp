efb62aab8c9d2d499618b0d5cd25f3177765c0db
maps <maps@techmaven.net>
2025-05-07 20:33:07 +0000

    CORS issue enabled

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+    reactStrictMode: true,
+    
+    
+    allowedDevOrigins: [
+    '3000-idx-arcgiswfs-t-queryt-1746548914037.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev'
+    ]
 };
 
 export default nextConfig;
